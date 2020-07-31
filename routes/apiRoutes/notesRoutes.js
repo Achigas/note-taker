f69c8186678a1deb971563ed573b00bfe708f7de
AChigas <Achigas34@gmail.com>
2020-07-31 16:23:25 -0700

    uniqid added for random ID gen

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { createNewNote } = require("../../lib/notes");
 const { notes } = require("../../db/db");
+const uniqid = require("uniqid")
 
 router.get("/notes", (req, res) => {
     let results = notes
@@ -9,6 +10,7 @@ router.get("/notes", (req, res) => {
 
 router.post("/notes", (req, res) => {
     //set id randomly
+    req.body.id = uniqid()
     const note = createNewNote(req.body, notes)
     res.json(note);
 })
